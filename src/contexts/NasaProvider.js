8d7c0e6cb453cb617ada70f0b9b926934b85d2f6
ehsan.jso <yek.jso@gmail.com>
2022-01-10 20:52:01 -0500

    Fetch data for last ten days

--- src/contexts/NasaProvider.js
+++ src/contexts/NasaProvider.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import { format, subDays } from "date-fns";
 import { host } from "../consts/host";
 import { useNavigate } from "react-router-dom";
 
@@ -15,13 +16,17 @@ export function NasaProvider({ children }) {
   const [fetchInProg, setFetchInProg] = useState(true);
   const apiKey = process.env.REACT_APP_NASA_KEY;
 
+  const today = format(new Date(), "yyyy-MM-dd");
+  const startDate = format(subDays(new Date(), 10), "yyyy-MM-dd");
+  const endDate = today;
+
   useEffect(() => {
     async function getNasa() {
       try {
         const { data } = await axios.get(
-          `${host}/planetary/apod?api_key=${apiKey}`
+          `${host}/planetary/apod?api_key=${apiKey}&start_date=${startDate}&end_date=${endDate}`
         );
-        console.log(data);
+        setPics(data);
       } catch (error) {}
       setFetchInProg(false);
     }
